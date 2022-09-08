7c55bb9ab8f4dd8dcdf3b4ed0ee38cc9f1084bce
Mauricio Junior <mjrfmoraes@outlook.com>
2022-09-08 16:30:07 -0400

    Exercicio de fixacao exececao concluido

---
+++ Exercicio Fixacao Excecoes - Aula 155/Entities/Exceptions/AccountException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Exercicio_Fixacao_Excecoes___Aula_155.Entities.Exceptions
+{
+    class AccountException:ApplicationException
+    {
+        public AccountException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Exercicio Fixacao Excecoes - Aula 155/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using Exercicio_Fixacao_Excecoes___Aula_155.Entities;
+using Exercicio_Fixacao_Excecoes___Aula_155.Entities.Exceptions;
+
+namespace Exercicio_Fixacao_Excecoes___Aula_155
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account Data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial Balance: ");
+                double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw Limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Account acc = new Account(number, holder, initialBalance, withdrawLimit);
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double withdrawAmount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                acc.Withdraw(withdrawAmount);
+                Console.WriteLine("New Balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch(AccountException e)
+            {
+                Console.WriteLine("Withdraw error: "+e.Message);
+            }
+        }
+    }
+}
